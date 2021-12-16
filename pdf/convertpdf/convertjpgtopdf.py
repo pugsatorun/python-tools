9a9409613aef4bca89cc7f97248a19d131d31bb8
pugsatoshi <satoshi.ito.dev@gmail.com>
2021-12-16 12:38:37 +0900

    repair

--- pdf/convertpdf/convertjpgtopdf.py
+++ pdf/convertpdf/convertjpgtopdf.py
@@ -1,16 +1,20 @@
 import os
+import pathlib
 import img2pdf
 import sys
 from PIL import Image 
 
 if __name__=='__main__':
-    output_Folder = '/home/satoshi/lab/python-tools/pdf/convertpdf/output/' #sys.argv[0] + '.pdf'
-    jpg_Folder = '/home/satoshi/satoshi/Desktop/'
-    png_folder = "/home/satoshi/satoshi/Desktop/" #sys.argv[1]
+    output_Folder = pathlib.Path('./output/') #sys.argv[0] + '.pdf'
+    output_Folder = str(output_Folder.resolve())
+    print("output_Folder :",output_Folder)
+    jpg_Folder = pathlib.Path(input("相対パスを入力:"))
+    jpg_Folder = str(jpg_Folder.resolve())
+    print("jpg_Folder :",jpg_Folder)
     extention = '.jpg'
-    png_file = [j for j in os.listdir(png_folder)if j.endswith(extention)]
-    print( png_file )
-    for j in png_file:
+    jpg_file = [j for j in os.listdir(jpg_Folder)if j.endswith(extention)]
+    print( jpg_file )
+    for j in jpg_file:
         output_FileName = j.replace(extention, '.pdf') #任意の文字列入れ替え
-        with open(output_Folder + output_FileName,'wb') as f:
-            f.write(img2pdf.convert(Image.open(png_folder + j).filename)) #PDFファイル生成
+        with open(output_Folder + '/' + output_FileName,'wb') as f:
+            f.write(img2pdf.convert(Image.open(jpg_Folder + '/' + j).filename)) #PDFファイル生成
